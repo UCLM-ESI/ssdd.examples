54e25aea9b23294cb4aba18f53e2888362515579
Soledad Escolar soledad escolar <Soledad Escolar soledad.escolar@uclm.es>
2017-10-16 16:55:31 +0200

    Corregido factorial_server.c

--- onc-rpc-factorial/factorial_server.c
+++ onc-rpc-factorial/factorial_server.c
@@ -1,10 +1,11 @@
 #include "factorial.h"
 
 u_long *factorial_1_svc(int *n, struct svc_req *rqstp) {
-    static u_long result=1;
+    static u_long result;
 
     printf("Received: n=%d \n", *n);
 
+    result=1;
     for (int i=*n; i>0; i--)
         result *= i;
 
