151a2d01ca12f33d59d444e40218e639d14eed4d
David Villa Alises <david.villa@uclm.es>
2017-10-17 13:49:51 +0200

    ZeroC Ice example

---
+++ ice/Client.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+import Ice
+Ice.loadSlice('Printer.ice')
+import Example
+
+
+class Client(Ice.Application):
+    def run(self, argv):
+        proxy = self.communicator().stringToProxy(argv[1])
+        printer = Example.PrinterPrx.checkedCast(proxy)
+
+        if not printer:
+            raise RuntimeError('Invalid proxy')
+
+        printer.write('Hello World!')
+
+        return 0
+
+
+sys.exit(Client().main(sys.argv))
