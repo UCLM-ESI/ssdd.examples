7a710221a3d0c208b8b0d7e795f030ee74c4bef5
David Villa Alises <david.villa@uclm.es>
2019-11-06 12:16:46 +0100

    fix format

--- rabbit-hello-world/receiver.py
+++ rabbit-hello-world/receiver.py
@@ -3,13 +3,15 @@
 
 import pika
 
+
 def callback(ch, method, properties, body):
-	print("[x] Received: %r" % body.decode("UTF-8"));
+    print("[x] Received: %r" % body.decode("UTF-8"))
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'));
-channel = connection.channel();
-channel.queue_declare(queue='hello');
-channel.basic_consume(callback, queue='hello', no_ack=True);
 
-print("[*] Waiting for messages. To exit press Ctrl+");
-channel.start_consuming();
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+channel.queue_declare(queue='hello')
+channel.basic_consume(callback, queue='hello', no_ack=True)
+
+print("[*] Waiting for messages. press Ctrl+C to exit")
+channel.start_consuming()
