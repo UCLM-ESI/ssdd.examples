8375f5cee38077e326d4e5d54dc0c5038136cbf3
David Villa Alises <david.villa@uclm.es>
2019-11-06 12:12:28 +0100

    receiver

--- rabbit-hello-world/receiver.py
+++ rabbit-hello-world/receiver.py
@@ -3,17 +3,13 @@
 
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'));
-channel = connection.channel();
-channel.queue_declare(queue='hello');
-
-
 def callback(ch, method, properties, body):
 	print("[x] Received: %r" % body.decode("UTF-8"));
 
-
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'));
+channel = connection.channel();
+channel.queue_declare(queue='hello');
 channel.basic_consume(callback, queue='hello', no_ack=True);
-
 
 print("[*] Waiting for messages. To exit press Ctrl+");
 channel.start_consuming();
