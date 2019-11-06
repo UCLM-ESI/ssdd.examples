97518643550fdc1fec8a792d8030fd635101cd4b
David Villa Alises <david.villa@uclm.es>
2019-11-06 12:46:00 +0100

    formating

--- rabbit-round-robin/producer.py
+++ rabbit-round-robin/producer.py
@@ -4,18 +4,19 @@
 import sys
 import pika
 
-connection=pika.BlockingConnection(pika.ConnectionParameters(host='localhost'));
-channel=connection.channel();
-
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
 channel.queue_declare(queue='task_queue', durable=True)
 
+message = ' '.join(sys.argv[1:]) or "Hello world!"
 
-message=' '.join(sys.argv[1:]) or "Hello world!"
+channel.basic_publish(exchange='',
+                      routing_key='task_queue',
+                      body=message,
+		      properties=pika.BasicProperties(
+                          delivery_mode = 2  # make message persistent
+                      ))
 
-
-channel.basic_publish(exchange='', routing_key='task_queue', body=message,
-			properties=pika.BasicProperties(delivery_mode = 2)#make message persistent
-			)
-
-print("[x] Sent: ", message);
-connection.close();
+print("[x] Sent: ", message)
+connection.close()
