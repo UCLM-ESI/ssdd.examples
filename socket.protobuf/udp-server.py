90df574caad14420559de35025027cb1b6c716d7
David Villa Alises <david.villa@gmail.com>
2020-09-20 18:13:44 +0200

    migrated to proto3

--- socket.protobuf/udp-server.py
+++ socket.protobuf/udp-server.py
@@ -3,13 +3,13 @@
 import socket
 from sensor_pb2 import Reading
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock = socket.socket(type=socket.SOCK_DGRAM)
 sock.bind(('', 2000))
 reading = Reading()
 
 while True:
     data, address = sock.recvfrom(1024)
-    print("sensor address: {}, raw-data: {}".format(address, data))
+    print(f"sensor: {address}, raw-data: {data}")
 
     reading.ParseFromString(data)
     print("Sensor {0.Id} ({1}) value:{0.value:.2f} {0.unit}".format(
