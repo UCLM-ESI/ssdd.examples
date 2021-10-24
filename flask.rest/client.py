3de2994aaefea59f46829b68492b974acc607359
José Luis Segura Lucas <profesor.jlsegura@uclm.es>
2021-10-24 02:19:39 +0200

    Modify Flask example to switch the status of the resource

--- flask.rest/client.py
+++ flask.rest/client.py
@@ -1,7 +1,18 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # -*- coding:utf-8; mode:python -*-
 
 from requests import put, get
 
-put('http://localhost:5000/device1', data={'status': 'enabled'})
-print(get('http://localhost:5000/device1').json())
+
+device_id = "device1"
+device_response = get(f'http://localhost:5000/{device_id}')
+
+if device_response.status_code == 404 or device_response.json()[device_id] == 'disabled':
+    new_status = 'enabled'
+
+else:
+    new_status = 'disabled'
+
+
+put(f'http://localhost:5000/{device_id}', data={'status': new_status})
+print(get(f'http://localhost:5000/{device_id}').json())
