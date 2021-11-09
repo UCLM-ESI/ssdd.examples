eef854f373f91d080ede2a4f839f665c9254bc59
David Villa Alises <david.villa@gmail.com>
2021-11-09 16:03:32 +0100

    another topic

---
+++ mqtt/publisher-humidity.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# -*- coding: utf-8; mode: python -*-
+
+import json
+import time
+import random
+import paho.mqtt.client as mqtt
+
+
+def take_reading():
+    return {
+        'identifier': 'X002',
+        'value': random.randint(20, 40),
+        'unit': 'Celsius',
+        'timestamp': time.time(),
+    }
+
+
+publisher = mqtt.Client()
+publisher.connect('127.0.0.1')
+
+while 1:
+    publisher.publish('humidity/X001', json.dumps(take_reading()))
+    time.sleep(1)
